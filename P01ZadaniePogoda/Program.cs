90651b88a6fdf685e26089ffebc890afb3a0437c
kurs <kurs@DESKTOP-V3HDKPQ>
2022-10-27 09:09:23 +0200

    Add project files.

---
+++ P01ZadaniePogoda/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01ZadaniePogoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string szukanyZnak = "°";
+                string znakKoncowy = ">";
+
+                Console.WriteLine("Podaj nazwe miasta");
+                string miasto = Console.ReadLine();
+                string adres = $"https://www.google.com/search?q=pogoda {miasto}";
+
+                WebClient wc = new WebClient();
+                string dane = wc.DownloadString(adres);
+
+                int indx = dane.IndexOf(szukanyZnak);
+                int aktuanaPozycja = indx;
+
+                while (dane.Substring(aktuanaPozycja, 1) != znakKoncowy)
+                    aktuanaPozycja--;
+
+                string wynik = dane.Substring(aktuanaPozycja + 1, indx - aktuanaPozycja + 1);
+
+                Console.WriteLine(wynik);
+            }
+           
+        }
+    }
+}
---
+++ P02WebClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02WebClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podanj nazwe miasta");
+
+            WebClient wc = new WebClient();
+
+            string s = wc.DownloadString("https://www.google.com/search?q=warszawa+pogoda");
+
+            File.WriteAllText("c:\\dane\\strona.html",s);
+
+            //Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
